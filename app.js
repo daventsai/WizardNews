dcd4b583658713254d84b038439d3c7aeff3a841
daventsai <daventsai75@gmail.com>
2023-05-03 10:39:37 -0500

    done-ish

--- app.js
+++ app.js
@@ -1,10 +1,71 @@
 const express = require("express");
+const morgan = require('morgan');
+const postBank = require('./postBank');
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+app.use(express.static('public'));
 
-const PORT = 1337;
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css"/>
+    </head>
+    <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`).join('')}
+      </div>
+    </body>
+  </html>`;
+  res.send(html);
+});
+
+app.get( '/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  const html=`<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css"/>
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div>
+        <h3>${post.title} by ${post.name}<h3>
+        <p>${post.content}</p>
+      </div>
+    </body>
+  </html>
+  `
+  if (!post.id){
+    throw new Error('Not found');
+  }
+  else{
+    res.send(html);
+  }
+  
+});
+
+
+const {PORT = 1337} = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
